aa165c1e725b5846c1efc54625c553c8af1bd7d8
Jefta Dirksen <JeftaDirksen@gmail.com>
2020-11-14 17:41:56 +0100

    Filters OldFiles and EmptyDirectories

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -15,27 +15,22 @@ function Cleanup($path) {
     [int] $deletedBytes = 0
     [int] $deletedDirectories = 0
 
-    $files = Get-ChildItem -Path $path -File -Recurse
-    foreach ($file in $files) {
-        [int]$ageCreated = (New-TimeSpan $file.CreationTime).TotalDays
-        [int]$ageWritten = (New-TimeSpan $file.LastWriteTime).TotalDays
-        $age = [Math]::Min($ageCreated, $ageWritten)
-
-        if ($age -ge $AgeInDays) {
-            if ($WhatIf.IsPresent) {
-                "WhatIf: Delete file '{0}' (Age: {1} Size: {2})" -f $file.FullName, $age, (DisplayInBytes $file.Length)
-            }
-            else {
-                "Delete file '{0}' (Age: {1} Size: {2})" -f $file.FullName, $age, (DisplayInBytes $file.Length)
-                #$file.Delete()
-            }
-            $deletedFiles++
-            $deletedBytes += $file.Length
+    # Cleanup Files
+    Get-ChildItem -Path $path -File -Recurse | OldFiles | ForEach-Object {
+        if ($WhatIf.IsPresent) {
+            "WhatIf: Delete file '{0}' (Age: {1} Size: {2})" -f $_.FullName, $_.Age, (FormatBytes $_.Length)
         }
+        else {
+            #$_.Delete()
+            "Deleted file '{0}' (Age: {1} Size: {2})" -f $_.FullName, $_.Age, (FormatBytes $_.Length)
+        }
+        $deletedFiles++
+        $deletedBytes += $_.Length
     }
 
-    Get-ChildItem $path -Directory -Recurse | Where-Object { (Get-ChildItem $_.FullName).Count -eq 0 } | ForEach-Object {
-        if ($WhatIf.IsPresent) {
+    # Cleanup Directories
+    Get-ChildItem $path -Directory -Recurse | EmptyDirectories | ForEach-Object {
+        if ($WhatIf.IsPresent) { 
             "WhatIf: Delete directory '{0}'" -f $_.FullName
         }
         else {
@@ -45,19 +40,31 @@ function Cleanup($path) {
         $deletedDirectories++
     }
 
-    if ($WhatIf.IsPresent) { "WhatIf: Delete {0} files ({1}) and {2} directories" -f $deletedFiles, (DisplayInBytes $deletedBytes), $deletedDirectories }
-    else { "Deleted {0} files ({1}) and {2} directories" -f $deletedFiles, (DisplayInBytes $deletedBytes), $deletedDirectories }
+    # Summery
+    if ($WhatIf.IsPresent) { "WhatIf: Delete {0} files ({1}) and {2} directories" -f $deletedFiles, (FormatBytes $deletedBytes), $deletedDirectories }
+    else { "Deleted {0} files ({1}) and {2} directories" -f $deletedFiles, (FormatBytes $deletedBytes), $deletedDirectories }
 
 }
 
-function DisplayInBytes($num) {
+filter OldFiles {
+    [int] $ageCreated = (New-TimeSpan $_.CreationTime).TotalDays
+    [int] $ageWritten = (New-TimeSpan $_.LastWriteTime).TotalDays
+    $_ | Add-Member -NotePropertyName Age -NotePropertyValue ([Math]::Min($ageCreated, $ageWritten))
+    if ($_.Age -ge $AgeInDays) { $_ }
+}
+
+filter EmptyDirectories {
+    if ((Get-ChildItem $_.FullName -Recurse -File -Attributes Archive,Normal,Hidden,System).Count -eq 0) { $_ }
+}
+
+function FormatBytes($bytes) {
     $suffix = "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"
     $index = 0
-    while ($num -gt 1kb) {
-        $num = $num / 1kb
+    while ($bytes -gt 1kb) {
+        $bytes = $bytes / 1kb
         $index++
     } 
-    "{0:N0} {1}" -f $num, $suffix[$index]
+    "{0:N0} {1}" -f $bytes, $suffix[$index]
 }
 
 main
